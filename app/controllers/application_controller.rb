6ada6fecb4e91b652b6ede4e5c96675655ec33d6
koyama <koyama1058tk@gmail.com>
2021-05-10 10:36:47 +0900

    ユーザーの登録要素を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :congigure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def congigure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zip, :address, :introduction])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_070833) do
+ActiveRecord::Schema.define(version: 2021_05_10_005031) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2021_05_07_070833) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.integer "zip"
+    t.string "address"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
