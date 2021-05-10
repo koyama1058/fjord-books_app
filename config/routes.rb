98f4b39a365d29334c39dd5a85124a04d1b8b5fb
koyama <koyama1058tk@gmail.com>
2021-05-10 11:45:37 +0900

    自身のアカウントを編集できる機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root "books#index"
   devise_for :users
   resources :books
-  resources :users, :only => [:index, :show]
+  resources :users, :only => [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
