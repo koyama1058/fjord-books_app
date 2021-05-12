5e6ceaf19016121ee5566ce3d089f579a835bf82
koyama <koyama1058tk@gmail.com>
2021-05-12 09:33:19 +0900

    rubocop

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Users::RegistrationsController < Devise::RegistrationsController
   def build_resource(hash = {})
     hash[:uid] = User.create_unique_string
@@ -8,6 +10,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def update_resource(resource, params)
     return super if params['password']&.present?
+
     resource.update_without_password(params.except('current_password'))
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
 
-         validates :email, presence: true, uniqueness: true
-         validates :uid, uniqueness: { scope: :provider }
+  validates :email, presence: true, uniqueness: true
+  validates :uid, uniqueness: { scope: :provider }
 
   def self.find_for_github_oauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name 
+      user.name = auth.info.name
     end
   end
 
