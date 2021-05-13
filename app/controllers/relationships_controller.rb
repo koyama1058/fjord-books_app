e34119b1f18cd7af489c615eecd89ad9455714f9
koyama <koyama1058tk@gmail.com>
2021-05-13 13:17:32 +0900

    rubocopを再修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@
 
 class RelationshipsController < ApplicationController
   def create
-    relationship = Relationship.create(follower_id: current_user.id, followed_id: params[:followed_id])
+    Relationship.create(follower_id: current_user.id, followed_id: params[:followed_id])
     redirect_to User.find(params[:followed_id])
   end
 
