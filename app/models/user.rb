803d226e7a358a6540b133ccf6d1c0c3331e540b
koyama <koyama1058tk@gmail.com>
2021-05-14 09:27:10 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
   has_many :reports
+  has_many :comments
   
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
