b1e31679fe5c0ef5648bf16883f688347f82c0ed
koyama <koyama1058tk@gmail.com>
2021-05-23 13:30:59 +0900

    relationship関係を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_040619) do
+ActiveRecord::Schema.define(version: 2021_05_23_043003) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -56,14 +56,6 @@ ActiveRecord::Schema.define(version: 2021_05_23_040619) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_friendships_on_followed_id"
     t.index ["follower_id"], name: "index_friendships_on_follower_id"
-  end
-
-  create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id", null: false
-    t.integer "followed_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
